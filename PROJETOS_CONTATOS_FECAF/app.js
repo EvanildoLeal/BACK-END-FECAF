775f291db3638daf6b3d995852c743491eef6e5d
EvanildoLeal <evanildo@wfxky.onmicrosoft.com>
2025-11-16 16:44:48 -0300

    PROJETO LIMPO: remove node_modules e arquivos desnecessários

---
+++ PROJETOS_CONTATOS_FECAF/app.js
@@ -0,0 +1,8 @@
+//MVC - (MODEL, VIEW, CONTROLLER)
+/*
+
+    Model - Modelagem dos dados (MD)Banco de Dados)
+    Controller - É responsável pela Regra de Negócio do Projeto
+    View - É responsável pela interação com o cliente(usuário) (Interface Gráfica)
+
+*/
